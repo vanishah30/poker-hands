a172bf920b3102c5ab7bf8f8bc65fe9de5500d91
Vanishah30 <vanisha.shah@gmail.com>
2023-04-19 14:41:09 +0200

    Straight flush test passed

---
+++ tests/isStraightFlush.test.js
@@ -0,0 +1,13 @@
+const Hand = require('../Hand');
+const CompareHands = require('../CompareHands');
+const suits = '♥♦♣♠';
+
+test('Test that sStraightFlush returns truthy is straight flush', () => {
+    let hand = new Hand('♥T', '♥9', '♥8', '♥7', '♥6');
+    expect(CompareHands.isStraightFlush(hand)).toBeTruthy();
+});
+
+test('check that sStraightFlush  returns falsey if not straight flush', () => {
+    let hand = new Hand('♣7', '♣6', '♥4', '♣8', '♣7');
+    expect(CompareHands.isStraightFlush(hand)).toBeFalsy();
+});
