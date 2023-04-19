eaadfc02af92c49ac983de04061e6f1d82d0b076
Vanishah30 <vanisha.shah@gmail.com>
2023-04-19 14:41:26 +0200

    Three of a kind test passed

--- tests/isThreeOfAKind.test.js
+++ tests/isThreeOfAKind.test.js
@@ -3,6 +3,11 @@ const CompareHands = require('../CompareHands');
 const suits = '♥♦♣♠';
 
 test('Test that threeOfAKind returns truthy is three of a kind', () => {
-  let hand = new Hand('♥7', '♦2', '♣7', '♠3', '♠7');
+  let hand = new Hand('♥4', '♦4', '♣4', '♠3', '♠7');
   expect(CompareHands.isThreeOfAKind(hand)).toBeTruthy();
+});
+
+test('check that isFlush returns falsey if not flush', () => {
+  let hand = new Hand('♣2', '♣6', '♥9', '♣8', '♣7');
+  expect(CompareHands.isFlush(hand)).toBeFalsy();
 });
