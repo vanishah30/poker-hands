a9da825a5d0c90ef2c96a53e537e3e4b25f67c4a
Vanishah30 <vanisha.shah@gmail.com>
2023-04-19 15:45:07 +0200

    Two hands compare in highest card

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const CompareHands = require('./CompareHands');
 // ♥♦♣♠
 
 let hand1 = new Hand('♥9', '♥T', '♥J', '♥Q', '♥K');
-let hand2 = new Hand('♣9', '♣T', '♣J', '♦Q', '♣K');
-console.log(CompareHands.comparer(hand1, hand2));
+let hand2 = new Hand('♣9', '♣5', '♣J', '♦Q', '♣K');
+console.log(CompareHands.compare(hand1, hand2));
 
